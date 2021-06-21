ed1716538323244ecf6265620fab6b77010b5fcb
AMFahim <amfahim2019@gmail.com>
2021-06-21 08:01:30 +0600

    burj al arab client almost done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDF25ksegxfTxIdRkalrY8cVqVZ0qoyY3Y",
+  authDomain: "burj-al-arab-9cc1f.firebaseapp.com",
+  projectId: "burj-al-arab-9cc1f",
+  storageBucket: "burj-al-arab-9cc1f.appspot.com",
+  messagingSenderId: "775086409137",
+  appId: "1:775086409137:web:50b0b5b65886c5d4b66990"
+};
+  export default firebaseConfig;
